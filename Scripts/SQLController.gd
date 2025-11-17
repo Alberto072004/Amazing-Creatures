1ad762de01935c2227a76646d41f3069ba5ba3f0
Alberto072004 <futbolerostratus@gmail.com>
2025-11-17 15:04:24 +0100

    17/11 - 15:04

--- Scripts/SQLController.gd
+++ Scripts/SQLController.gd
@@ -16,6 +16,10 @@ var hp = [
 	20, 10, 20, 25, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20
 ]
 
+var speed = [
+	30, 35, 25, 15, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	database = SQLite.new() #Creamos la Base de datos
@@ -29,11 +33,22 @@ func createTable() -> void:
 		"id" = {"data_type":"int", "primary_key":true, "not_null":true, "auto_increment":true},
 		"name" = {"data_type":"text"},
 		"hp" = {"data_type":"int"},
-		"attack" = {"data_type":"int"},
-		"defense" = {"data_type":"int"},
 		"speed" = {"data_type":"int"}
 	}
 	database.create_table("Criatures", criaturesTable)
+	
+	var typeTable = {
+		"id" = {"data_type":"int", "primary_key":true, "not_null":true, "auto_increment":true},
+		"name" = {"data_type":"text"}
+	}
+	database.create_table("Type", typeTable)
+	
+	var movementsTable = {
+		"id" = {"data_type":"int", "primary_key":true, "not_null":true, "auto_increment":true},
+		"name" = {"data_type":"text"},
+		"damage" = {"data_type":"int"}
+	}
+	database.create_table("Movements", movementsTable)
 	
 func insertCriatures() -> void:
 	# Comprueba cuántas criaturas hay ya en la tabla
@@ -43,12 +58,12 @@ func insertCriatures() -> void:
 		return
 	# Si está vacía, insertamos
 	# for nombre in criatures:
-	#	database.insert_row("Criatures", {"name": nombre, "hp":20})
+	#	database.insert_row("Criatures", {"name": nombre)
 	for i in range(criatures.size()):
 		var nombre = criatures[i]
 		var puntos_vida = hp[i]
-		database.insert_row("Criatures", {"name": nombre, "hp": puntos_vida})
-
+		var velocidad = speed[i]
+		database.insert_row("Criatures", {"name": nombre, "hp": puntos_vida, "speed": velocidad})
 	print("Criaturas insertadas correctamente.")
 
 
